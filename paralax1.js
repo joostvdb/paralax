56329572df90711ad9bf6dfc9f444e9dec6926ed
joostvdb <joostvdb@users.noreply.github.com>
2020-04-11 23:13:59 +0200

    paralax googlyeyes and paralax leaves mouse movent and scrolling

---
+++ paralax1.js
@@ -0,0 +1,179 @@
+//html setup
+
+var itemsHtmlColletion = document.getElementsByClassName('paralax-item');
+var itemsA = Array.from(itemsHtmlColletion);
+var html = document.documentElement
+
+var scrollAmt = html.scrollTop;
+var scrollMax = html.scrollHeight - window.innerHeight;
+
+
+//console.log ('pupilsA', pupilsA);
+
+
+// input
+var input = {
+    scrollY: {
+        start: 0,
+        end: html.scrollHeight - window.innerHeight,
+        current: 0,
+
+    },
+
+
+    mouseX: {
+        start: 0,
+        end: window.innerWidth,
+        current: 0,
+
+    },
+
+    mouseY: {
+
+        start: 0,
+        end: window.innerHeight,
+        current: 0,
+
+    },
+}
+input.scrollY.range = input.scrollY.end - input.scrollY.start;
+input.mouseX.range = input.mouseX.end - input.mouseX.start;
+input.mouseY.range = input.mouseY.end - input.mouseY.start;
+
+// output
+
+var output = {
+    X: {
+        start: -150,
+        end: 150,
+        current: 0,
+
+    },
+    Y: {
+        start: -150,
+        end: 150,
+        current: 0,
+
+    },
+    zIndex: {
+        range: 10000,
+    },
+    scale: {
+        start: 1,
+        end: 0.2,
+
+    },
+    blur: {
+        startingdepth: 0.2,
+        range:20,
+
+    },
+}
+output.scale.range = output.scale.end - output.scale.start
+output.X.range = output.X.end - output.X.start
+output.Y.range = output.Y.end - output.Y.start
+
+var mouse = {
+    x: window.innerWidth / 0.5,
+    y: window.innerHeight / 0.5,
+}
+
+// mouse X Y
+
+var updateInputs = function() {
+    // mouse X input
+    input.mouseX.current= mouse.x;
+    input.mouseX.fraction= (input.mouseX.current - input.mouseX.start)/ input.mouseX.range;
+    // mouse Y input
+    input.mouseY.current= mouse.y;
+    input.mouseY.fraction= (input.mouseY.current - input.mouseY.start)/ input.mouseY.range;
+    // scroll y input
+    input.scrollY.current = html.scrollTop;
+    input.scrollY.fraction= (input.scrollY.current - input.scrollY.start)/ input.scrollY.range;
+
+}
+
+var updateOutputs = function() {
+
+    // output.X.current= output.X.end - (input.mouseX.fraction * output.X.range);
+    
+    // output.Y.current= output.Y.end - (input.mouseY.fraction * output.Y.range);
+
+    output.Y.current= output.Y.end - (input.scrollY.fraction * output.Y.range);
+
+
+
+}
+
+var updateEachparalaxitem = function() {
+    
+      //apply output to html
+
+    itemsA.forEach(function (item, k) {
+        var depth = parseFloat(item.dataset.depth, 10);
+        var itemOutput = {
+            x: output.X.current-(output.X.current * depth),
+            y: output.Y.current-(output.Y.current * depth),
+            zIndex: output.zIndex.range - (output.zIndex.range * depth),
+            scale: output.scale.start + (output.scale.range * depth),
+            blur: (depth - output.blur.startingdepth)*output.blur.range,
+    }
+    console.log (k,'depth', depth)
+    item.style.filter = 'blur('+itemOutput.blur+'px)'
+    item.style.zIndex = itemOutput.zIndex
+    item.style.transform = 'scale('+itemOutput.scale+') translate('+itemOutput.x+'px,'+itemOutput.y+'px)';
+
+
+
+});
+
+}
+
+var handleMouseMove = function(event) {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+
+    updateInputs();
+    updateOutputs();
+    updateEachparalaxitem();
+
+    // if (input.mouseX.fraction > 1) {
+    //     input.mouseX.fraction = 1;
+    // }
+    // if (input.mouseX.fraction <0) {
+    //     input.mouseX.fraction = 0;
+    // }
+    //console.log('output.x.current', output.x.current);
+    //console.log('fraction Y', input.mouseY.fraction); 
+}
+
+var handleScroll = function() {
+    updateInputs();
+    updateOutputs();
+    updateEachparalaxitem();
+    
+    var scrollMax = document.documentElement.scrollHeight - window.innerHeight;
+    // console.log ('scrolling!', scrollAmt, 'scrolmax', scrollMax)
+}
+
+var handleResize = function() {
+    input.mouseX.end = (window.innerWidth)
+    input.mouseX.range = input.mouseX.end - input.mouseX.start;
+    input.mouseY.end = (window.innerHeight)
+    input.mouseX.range = input.mouseX.end - input.mouseX.start;
+    input.scrollY.end = html.scrollHeight - window.innerHeight;
+    input.scrollY.range = input.scrollY.end - input.scrollY.start;
+}
+
+
+
+    
+// eventlistener
+
+window.addEventListener('mousemove', handleMouseMove)
+window.addEventListener('resize', handleResize)
+document.addEventListener('scroll', handleScroll)
+
+updateInputs();
+updateOutputs();
+updateEachparalaxitem();
